99f3a432380e06bf4a3965d99073c809a4bd0289
MarioJCaro <mario.caro@estudiantes.utec.edu.uy>
2024-07-02 17:10:43 -0300

    Datos de prueba

--- backend-master/datos/dataPrIns.js
+++ backend-master/datos/dataPrIns.js
@@ -1,11 +1,14 @@
 let productos = [
-    { id: 1, nombre: 'Producto 1', descripcion: 'Descripción 1', imagen: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA', precio: 10.0, insumos: [{insumoId:1,cantidad:2}] },
-    { id: 2, nombre: 'Producto 2', descripcion: 'Descripción 2', imagen: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA', precio: 20.0, insumos: [] },
+    { id: 1, nombre: 'Pan 1/2 Kg', descripcion: '1/2 Kg pan felipe', imagen: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA', precio: 10.0, insumos: [{insumoId:1,cantidad:0.3}, {insumoId:3, cantidad:5}] },
+    { id: 2, nombre: 'Sanguches JyQ', descripcion: 'Una bandeja de 12 unidades.', imagen: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA', precio: 20.0, insumos: [{insumoId:4, cantidad: 0.1}, {insumoId:5, cantidad: 0.1}] },
 ];
 
 let insumos = [
     { id: 1, nombre: 'Harina', unidad: 'Kg', costo: 100 },
-    { id: 2, nombre: 'Sal', unidad: 'Kg', costo: 300 }
+    { id: 2, nombre: 'Sal', unidad: 'Kg', costo: 300 },
+    { id: 3, nombre: 'Levadura', unidad: 'Gr', costo: 200 },
+    { id: 4, nombre: 'Jamón', unidad: 'Kg', costo: 500 },
+    { id: 5, nombre: 'Queso', unidad: 'Kg', costo: 500 }
 ];
 
 let estados = [
